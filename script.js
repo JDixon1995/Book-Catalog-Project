e41b2e27d30ead115c066a462750883123be9ba4
John <jddhalogamer@gmail.com>
2021-07-15 15:59:51 -0500

    outlined project scope.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 //Book Catalog
 
+let myLibrary = [
+    book1, book2, book3
+];
+
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayLibrary() {
+    for(i = 0; i < myLibrary.length; i++) {
+
+    }
 }
 
